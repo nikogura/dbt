0e9ac755201b00a333efdaa63679f99b17f65490
Nik Ogura <nik@scribd.com>
2018-09-04 10:42:10 -0700

    fixed forgotten version in main.go

--- cmd/dbt/main.go
+++ cmd/dbt/main.go
@@ -12,7 +12,7 @@ import (
 const DBT = "/usr/local/bin/dbt"
 
 // VERSION the version of dbt.  Must match version in metadata.json
-const VERSION = "2.1.4"
+const VERSION = "2.1.5"
 
 // there are only two options for dbt itself, 'version' and 'offline'
 var version string
