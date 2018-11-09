2540823af94128b9268dcdaf641ac8b5a5013ba0
Nik Ogura <nik@scribd.com>
2018-11-09 12:04:23 -0800

    added to precommit hook to block cases where version in metadata do not match version in code

--- pre-commit-hook.sh
+++ pre-commit-hook.sh
@@ -1,2 +1,12 @@
 #!/usr/bin/env bash
 /usr/local/go/bin/gofmt -w ./
+
+METADATA_VERSION=$(grep version metadata.json | awk '{print $2}' | sed 's/[",]//g')
+
+CODE_VERSION=$(grep VERSION cmd/dbt/main.go | grep -v version | awk '{print $4}' | sed 's/"//g')
+
+if [[ ${METADATA_VERSION} != ${CODE_VERSION} ]]; then
+  echo "Versions do not match!"
+  echo "'VERSION' in cmd/dbt/main.go must match 'version' in metadata.json"
+  exit 1
+fi
